0d13d9211ddf45fbd991631b68ce5bbf319b8fb6
Samuel P. Gillispie II <Samuel.Gillispie@ibm.com>
2020-01-30 14:20:51 -0600

    Restyled avatar and switched out provider

--- src/components/PlayerAvatar.js
+++ src/components/PlayerAvatar.js
@@ -8,19 +8,16 @@ import * as global from "../config/state";
 const outerRingSize = "4px";
 
 const Container = styled.div`
+  box-sizing: border-box;
+
+  border: ${outerRingSize} solid
+    ${props =>
+      props.highlight ? "#FFD300" : props.theme.global.colors.text.light};
   border-radius: calc(${props => props.size} / 2);
   width: ${props => props.size};
   height: ${props => props.size};
 
-  background: ${props => (props.highlight ? "#FFD300" : "white")};
-
-  ${props =>
-    props.highlight &&
-    css`
-      > div {
-        border: 2px solid white;
-      }
-    `}
+  background: white;
 `;
 
 const Backdrop = styled.div`
@@ -28,12 +25,17 @@ const Backdrop = styled.div`
 
   overflow: hidden;
 
-  margin: ${outerRingSize};
+  /* margin: ${outerRingSize}; */
+  border: 2px solid white;
   border-radius: calc((${props => props.size} - ${outerRingSize}) / 2);
   width: calc(${props => props.size} - ${outerRingSize} * 2);
   height: calc(${props => props.size} - ${outerRingSize} * 2);
 
-  background: url(https://api.adorable.io/avatars/256/${props => props.playerId}.png);
+
+  /* background: url(https://api.adorable.io/avatars/256/${props =>
+    props.playerId}.png); */
+  background: url(https://avatars.dicebear.com/v2/human/${props =>
+    props.playerId}.svg);
   background-position: center;
   background-size: contain;
 
