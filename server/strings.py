90c613942af8688916b4f961a252ae11350c7bf5
Samuel Gillispie II <spgillispie2@gmail.com>
2022-06-21 12:45:54 -0400

    Small changes to strings

--- server/strings.py
+++ server/strings.py
@@ -4,12 +4,12 @@ import enum
 
 class Bundle(enum.Enum):
     # Error strings
-    ERROR_BANKER_CANNOT_LEAVE = "You are the banker. You cannot leave."
+    ERROR_BANKER_CANNOT_LEAVE = "You are the Banker. You cannot leave."
     ERROR_KICK_NOT_FOUND = "Target player not found"
     ERROR_KICK_YOURSELF = "You cannot kick yourself"
     ERROR_PLY_NAME_BLACKLIST = "That player name is not allowed"
     ERROR_PLY_NOT_ACTIVE = "You are no longer part of this lobby"
-    ERROR_PLY_NOT_BANKER = "You are not the banker!"
+    ERROR_PLY_NOT_BANKER = "You are not the Banker!"
     ERROR_LOBBY_CODE_INVALID = "Lobby with this code does not exist"
     ERROR_LOBBY_FULL = "This lobby is full"
     ERROR_LOBBY_EXPIRED = "This lobby has expired"
@@ -21,17 +21,15 @@ class Bundle(enum.Enum):
     ERROR_INVALID_OPTIONS = "Invalid game options"
 
     # Event strings
-    EVENT_PLY_JOIN = "{0} joined the game. The Bank gave them {1} to get started."
-    EVENT_PLY_MADE_BANKER = "{0} has been made The Banker."
+    EVENT_PLY_JOIN = "{0} joined the game. The Bank transferred to them a starting balance of {1}."
+    EVENT_PLY_MADE_BANKER = "{0} has been promoted to Banker."
     EVENT_PLY_TRANSFER_BANKER = (
         "{0} transferred Banker responsibilities to {1}."
     )
     EVENT_PLY_LEAVE = (
-        "A player has left. Their cash has been returned to the bank."
+        "A player has left. Their balance has been returned to the bank."
     )
-    EVENT_PLY_KICK = (
-        "A player has been kicked. Their cash has been returned to the bank."
-    )
+    EVENT_PLY_KICK = "A player has been kicked. Their balance has been returned to the bank."
     EVENT_TRANSFER = "{0} transferred {1} from {2} to {3}."
     EVENT_DISBANDED = "The lobby has been disbanded."
 
